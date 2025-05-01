3cc8bedbc2f202a964e139d7ff257be1b57e4436
pro <stephanedjatio@gmail.com>
2025-05-01 06:41:42 +0100

    Update new

--- src/app/directives/pokemon-border.directive.ts
+++ src/app/directives/pokemon-border.directive.ts
@@ -6,6 +6,7 @@ import {
   Renderer2,
   AfterViewInit,
 } from '@angular/core';
+import { getPokemonColor } from '../models/pokemon.model';
 
 @Directive({
   selector: '[appPokemonBorder]',
@@ -33,7 +34,7 @@ export class PokemonBorderDirective implements AfterViewInit {
   }
 
   @HostListener('mouseenter') onMouseEnter() {
-    const color = this.getBorderColor(this.pokemonType);
+    const color = getPokemonColor(this.pokemonType);
     this.setBorder(color);
   }
 
@@ -43,28 +44,5 @@ export class PokemonBorderDirective implements AfterViewInit {
 
   private setBorder(color: string) {
     this.renderer.setStyle(this.el.nativeElement, 'borderColor', color);
-  }
-
-  private getBorderColor(type: string): string {
-    switch (type) {
-      case 'Feu':
-        return '#EF5350';
-      case 'Eau':
-        return '#42A5F5';
-      case 'Plante':
-        return '#66BB6A';
-      case 'Insecte':
-        return '#8d6e63';
-      case 'Vol':
-        return '#90CAF9';
-      case 'Poison':
-        return '#b388ff';
-      case 'Fée':
-        return '#f8bbd0';
-      case 'Electrik':
-        return '#f4ff81';
-      default:
-        return '#303030';
-    }
   }
 }
--- src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
+++ src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
@@ -1,12 +1,18 @@
 import { Component, inject, Input, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../services/pokemon.service';
-import { DatePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { getPokemonColor } from '../../models/pokemon.model';
+import {
+  FormArray,
+  FormControl,
+  ReactiveFormsModule,
+  FormGroup,
+  FormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-pokemon-edit',
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, ReactiveFormsModule],
   templateUrl: './pokemon-edit.component.html',
   styleUrl: './pokemon-edit.component.css',
 })
@@ -27,19 +33,63 @@ export class PokemonEditComponent {
 
   readonly pokemonTypes = signal(this.#pokemonservice.getPokemonTypeList());
 
+  readonly form = new FormGroup({
+    name: new FormControl(this.pokemon().name),
+    life: new FormControl(this.pokemon().life),
+    damage: new FormControl(this.pokemon().damage),
+    types: new FormArray(
+      this.pokemon().types.map((type) => new FormControl(type))
+    ),
+  });
+
+  get pokemonTypeList(): FormArray {
+    return this.form.get('types') as FormArray;
+  }
+
+  isPokemonTypeSelected(type: string): boolean {
+    return !!this.pokemonTypeList.controls.find(
+      (control) => control.value == type
+    );
+  }
+
+  onPokemonTypeChange(type: string, isChecked: boolean) {
+    if (isChecked) {
+      const control = new FormControl(type);
+      this.pokemonTypeList.push(control);
+    } else {
+      const index = this.pokemonTypeList.controls
+        .map((control) => control.value)
+        .indexOf(type);
+
+      this.pokemonTypeList.removeAt(index);
+    }
+  }
+
+  getPokemonColor(type: string) {
+    return getPokemonColor(type);
+  }
+
   incrementLife() {
-    if (this.life < this.max) this.life++;
+    const current = this.form.get('life')?.value || 0;
+    if (current < this.max) this.form.get('life')?.setValue(current + 1);
   }
 
   decrementLife() {
-    if (this.life > this.min) this.life--;
+    const current = this.form.get('life')?.value || 0;
+    if (current > this.min) this.form.get('life')?.setValue(current - 1);
   }
 
   incrementDamage() {
-    if (this.damage < this.max) this.damage++;
+    const current = this.form.get('damage')?.value || 0;
+    if (current < this.max) this.form.get('damage')?.setValue(current + 1);
   }
 
   decrementDamage() {
-    if (this.damage > this.min) this.damage--;
+    const current = this.form.get('damage')?.value || 0;
+    if (current > this.min) this.form.get('damage')?.setValue(current - 1);
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
   }
 }
